27522dfab9368b1a71ccd8f88407e34fad2217e7
Rafa <raf141.92@gmail.com>
2017-11-08 23:23:33 +0100

    37 y 40 hoja 3

--- Pseudocodigo3-34/src/Pseudocodigo3_34.java
+++ Pseudocodigo3-34/src/Pseudocodigo3_34.java
@@ -8,10 +8,15 @@ public class Pseudocodigo3_34 {
 
 		for (i=0; i<arrayNotas.length; i++){
 			arrayNotas[i]=(int)Math.floor(Math.random()*10);
-			System.out.print((int)arrayNotas[i]);
+			System.out.printf("%3d", (int)arrayNotas[i]);
 			conta= conta + arrayNotas[i];
 		}
-		System.out.println("La media es: " +(float)conta/10);
+		System.out.println("\nLa media es: " +(float)conta/10); //salto de linea
+		if (((float)conta/10)>5){
+			System.out.print(" (Aprobado)");
+		}else{
+			System.out.print(" (Suspenso)");
+		}
 	}//main
 
 }//Class
