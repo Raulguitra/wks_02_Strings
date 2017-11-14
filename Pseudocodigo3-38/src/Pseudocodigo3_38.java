ce95aac41c343868437e1282815819d95b37ca02
rafa <raf141.92@gmail.com>
2017-11-14 16:55:00 +0100

    random

--- Pseudocodigo3-38/src/Pseudocodigo3_38.java
+++ Pseudocodigo3-38/src/Pseudocodigo3_38.java
@@ -5,15 +5,14 @@ public class Pseudocodigo3_38 {
 	public static void main(String[] args) {
 		final int alumno = 30;
 		final int asig = 10;
-		int Array1 [][],i,j,conta=0, numRandom; 
-		Array1 = new int[alumno][asig];
+		int i,j,conta=0, numRandom; 
+		int Array1[][] = new int[30][10];
 		Scanner sc = new Scanner(System.in);
 		
-		for (i=0;i<alumno; i++){
-			numRandom=
+		for (i=0;i<30; i++){
 			conta=0;
 			System.out.println();
-			for (j=0; j<asig; j++){
+			for (j=0; j<10; j++){
 				Array1 [i][j]=(int)Math.floor(Math.random()*11);
 				System.out.printf("%5d", Array1 [i][j]);
 				conta+=Array1[0][j];
--- Pseudocodigo3-41/src/Pseudocodigo3_41.java
+++ Pseudocodigo3-41/src/Pseudocodigo3_41.java
@@ -1,12 +1,18 @@
 public class Pseudocodigo3_41 {
 
 	public static void main(String[] args) {
-		int i,j,k, conta=0, max=0;
+		int i,j=0,k=0, conta=0, max=0, num1, num2, num3;
+		num1 =(int)Math.floor(Math.random()*11);
+		num2 =(int)Math.floor(Math.random()*11);
+		num3 =(int)Math.floor(Math.random()*11);
 		int tabla [][][] = new int[5][10][20];
+
 		for (i=0; i<5; i++){
+			conta=0;
 			System.out.println();
 			System.out.println("----------------------------------------------------------------------------------------------------");
 			for (j=0; j<10; j++){
+				conta += tabla[0][j][0];
 				System.out.println();
 				for (k=0; k<20; k++){
 					tabla[i][j][k]=(int)Math.floor(Math.random()*11);
@@ -15,6 +21,22 @@ public class Pseudocodigo3_41 {
 			}
 		}
 		System.out.println("\n----------------------------------------------------------------------------------------------------");
-	}//main
+		System.out.println("\n----------------------------------------------------------------------------------------------------");
 
-}//Classs
+		for (i=0; i<5; i++){
+			System.out.println();
+			System.out.println();
+			for (j=0; j<10; j++){
+				System.out.println("La nota media del curso #" +(j+1) +" del centro # " +(i+1) +" es " +(float)conta/10);
+				System.out.println();
+				for (k=0; k<20; k++){
+				}
+			}
+		}
+		System.out.println("\n----------------------------------------------------------------------------------------------------");
+
+
+
+	}//main
+	
+}//Class
