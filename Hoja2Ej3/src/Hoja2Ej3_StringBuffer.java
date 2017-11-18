8c54c5d0f44ebd6bd222644f372f8ede53277374
Rafa <raf141.92@gmail.com>
2017-11-18 10:31:18 +0100

    palindromo bien

--- Hoja2Ej3/src/Hoja2Ej3_StringBuffer.java
+++ Hoja2Ej3/src/Hoja2Ej3_StringBuffer.java
@@ -1,25 +1,19 @@
 /*Escribe un programa que pida por teclado una palabra y diga si es un palíndromo o no. 
 Una palabra es un palíndromo si se lee igual de izquierda a derecha que de derecha a izquierda*/
 import java.util.Scanner;
-public class Hoja2Ej3 {
+public class Hoja2Ej3_StringBuffer {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s1,s1reverse="";
-		int i;
+		String s1;
 		System.out.println("Escribe una frase");
 		s1 = sc.nextLine();
-		
-		for (i=s1.length()-1; i>=0; i--){
-			s1reverse+=s1.charAt(i);
-		}
-		System.out.println(s1reverse);	
-		
-		
-		if (s1.equals(s1reverse)){
+		StringBuffer sb = new StringBuffer(s1);
+		sb = sb.reverse(); //invertir contenido de la cadena
+		if (s1.equals(sb.toString())){
 			System.out.println("Es un palíndromo");
 		}else{
-		System.out.println("No es un palíndromo");
-	}
+			System.out.println("No es un palíndromo");
+		}
 	}//main
 }//Class
