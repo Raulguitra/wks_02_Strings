7ccc54d18c7f4fbfac2b4bec4871add376cebf33
Rafa <raf141.92@gmail.com>
2017-11-18 10:05:31 +0100

    replace si,no

--- Hoja2Ej6/src/Hoja2Ej6.java
+++ Hoja2Ej6/src/Hoja2Ej6.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+//Escribe un programa que pida por teclado una frase y cambie cada ocurrencia de la palabra “si” por la palabra “no”.
+//Después lo muestra por pantalla
 
+import java.util.Scanner;
 public class Hoja2Ej6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s1, palabra;
+		String s1;
 		System.out.println("Escribe una frase");
 		s1 = sc.nextLine();
 		System.out.println(s1.replace("si", "no"));
