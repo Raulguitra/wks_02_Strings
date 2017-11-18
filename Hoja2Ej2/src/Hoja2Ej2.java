afe86b15a3d9ebf6c403a817d7c88889a9ce7031
Rafa <raf141.92@gmail.com>
2017-11-18 09:56:07 +0100

    Eliminar 3 primeras ocurrencias de una letra

--- Hoja2Ej2/src/Hoja2Ej2.java
+++ Hoja2Ej2/src/Hoja2Ej2.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
 
+
+/*Escribe un programa que pida por teclado una cadena y una letra, 
+de manera que elimine las 3 primeras ocurrencias de la letra que encuentre en la cadena y lo muestre por pantalla. */
+
+import java.util.Scanner;
 public class Hoja2Ej2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s1;
 		int i, conta=0;
-		char a;
+		char letra;
 		System.out.println("Escribe una frase");
 		s1 = sc.nextLine();
 		System.out.println("Escribe una letra");
-		a = sc.next().charAt(0); //pedir char por teclado
+		letra = sc.next().charAt(0); //pedir char por teclado
 		for (i=0; i<s1.length(); i++){
-
-			if(s1.charAt(i)==a){
+			if(s1.charAt(i)==letra){
 				if(conta==0 || conta==1 || conta==2){
 					//Si es las primeras 3 no lo imprime
 				}
